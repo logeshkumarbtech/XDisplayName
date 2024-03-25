adaab5fb93cdf320f0401baadd13aa59c4bfbf9f
Logesh kumar G <logeshkumarbtech@gmail.com>
2024-03-25 15:24:33 +0530

    xdisplayname

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+// import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [obj, setObj] = useState({
+    firstname: "",
+    lastname: "",
+  });
+  const [fullname, setFullname] = useState("");
+
+  console.log(obj.firstname, obj.lastname);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (obj.firstname && obj.lastname) {
+      const newName = obj.firstname + " " + obj.lastname;
+      console.log(newName);
+      setFullname(newName);
+    }
+  };
+
+  const handleFistNameChange = (e) => {
+    setObj({ ...obj, firstname: e.target.value });
+  };
+
+  const handleLastNameChange = (e) => {
+    setObj({ ...obj, lastname: e.target.value });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstname">First Name:</label>
+          <input
+            type="text"
+            placeholder=""
+            value={obj.firstname}
+            onChange={handleFistNameChange}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="lastname">Last Name:</label>
+          <input
+            type="text"
+            placeholder=""
+            value={obj.lastname}
+            onChange={handleLastNameChange}
+          ></input>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {fullname && (
+        <div>
+          <p>Full Name: {fullname}</p>
+        </div>
+      )}
     </div>
   );
 }
